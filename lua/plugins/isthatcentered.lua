52561733ec96b88a4d8a4c845970f96593c31ae9
Edouard Penin <e.peninb@gmail.com>
2026-04-10 10:00:05 +0200

    Use refactor.nvim Lua API

--- lua/plugins/isthatcentered.lua
+++ lua/plugins/isthatcentered.lua
@@ -1,3 +1,18 @@
 return {
-  -- {dir = 	"/Users/edouardpenin/Test/nvim/acid.nvim", lazy = true, opts = { }, priority = 1000},
+  {
+    dir = '/Users/edouardpenin/Test/nvim/refactor.nvim',
+    name = 'refactor.nvim',
+    keys = {
+      {
+        '<leader>grq',
+        function()
+          local Refactor = require 'refactor'
+
+          Refactor.typescript.replace_with_template_string(Refactor.context())
+        end,
+        desc = 'Replace with template string',
+      },
+    },
+  },
+  -- { dir = '/Users/edouardpenin/Test/nvim/acid.nvim', lazy = true, opts = {}, priority = 1000 },
 }
